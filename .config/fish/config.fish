1e0942061d001c392ef1f4eac2e2f47bde041bdc
Infernalloo <infernodev00@gmail.com>
2023-01-30 23:23:59 +0100

    modified config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,16 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-## neofetch ##
-#neofetch
-
-## pfetch ##
-#pfetch
-
 ## starship ##
 starship init fish | source
 
 ## aliases ##
 source $HOME/.config/fish/aliases.txt
 
-fish_add_path /home/inferno/.spicetify
+## editor ##
+export EDITOR="/usr/bin/nvim"
+
+# fish_add_path /home/inferno/.spicetify
+
+# export PATHA="$HOME/.emacs.d/bin:$PATH"
