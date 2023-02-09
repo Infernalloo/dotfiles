64e2c3d830f20eb0779b0345c055ca8473912a52
Infernalloo <infernodev00@gmail.com>
2023-02-09 13:36:37 +0100

    modified config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,11 +7,8 @@ starship init fish | source
 
 ## aliases ##
 #source $HOME/.config/fish/dnf-aliases.txt
-source $HOME/.config/fish/pacman-aliases.txt
+#source $HOME/.config/fish/pacman-aliases.txt
 
 ## editor ##
 export EDITOR="/usr/bin/nvim"
 
-# fish_add_path /home/inferno/.spicetify
-
-# export PATHA="$HOME/.emacs.d/bin:$PATH"
